3bf3d50cde07d38b201005c8dde6d2f916f2ae15
danielorf <danielorf@gmail.com>
2017-12-06 16:22:44 -0800

    Added run.py and fixed app.py and uwsgi.ini

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = "jose"
+app.secret_key = "dan"
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity) # /auth
 
+api.add_resource(UserRegister, "/register")
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemList, "/items")
-api.add_resource(UserRegister, "/register")
 api.add_resource(Store, "/store/<string:name>")
 api.add_resource(StoreList, "/stores")
+
 
 
 if __name__ == "__main__":
